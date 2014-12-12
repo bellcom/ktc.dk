8e5942aad5d369af30309390c56dadec34510e74
Yani0029 <yani@bellcom.dk>
2014-12-12 15:14:11 +0100

    Added hearing node display along with other answer, invite pages. Added template to ktc theme.

---
+++ public_html/sites/all/themes/ktc/theme/hearing-answer.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ * Template for final answer page.
+ */
+?>
+<div class="row final-answer">
+  <div class="col-md-4 col-sm-4 col-xs-12">
+    <div class="hearing-preface">
+      <?php if (isset($preface)): ?>
+        <?php print $preface; ?>
+      <?php endif; ?>
+    </div>
+    <div class="responses-container">
+      <?php if (isset($responses)): ?>
+        <?php foreach ($responses as $response): ?>
+          <?php print render($response); ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+  <div class="col-md-5 col-sm-5 col-xs-12">
+    <?php if (isset($answer_form)): ?>
+      <?php print $answer_form ?>
+    <?php endif; ?>
+  </div>
+  <div class="col-md-3 col-sm-3 col-xs-12">
+    <?php $menu = menu_local_tabs(); print render($menu); ?>
+  </div>
+</div>
