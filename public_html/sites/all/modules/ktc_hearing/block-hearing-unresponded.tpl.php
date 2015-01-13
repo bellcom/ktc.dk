77913d399d3a9f9d6ab28e1ffe36cf81d6d81765
Thomas Thune Hansen <tth@bellcom.dk>
2015-01-13 10:33:29 +0100

    Added a block with users unresponded hearings

---
+++ public_html/sites/all/modules/ktc_hearing/block-hearing-unresponded.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Template for unresponded hearings block.
+ */
+?>
+<!-- block-hearing-unresponded.tpl.php -->
+<div class="view-content">
+  <? if (!empty($unresponded_hearings)): ?>
+    <? foreach($unresponded_hearings as $hearing): ?>
+
+    <div class="views-row">
+      <div class="created"><?php print date('d. F Y', $hearing->created); ?></div>
+      <h5 class="field-content"><? print l($hearing->title, 'node/' . $hearing->nid); ?></h5>
+
+      <span class="field-content">Af <? print l($hearing->user_mail, 'user/' . $hearing->uid, array('attributes' => array('class' => array('username'), 'title' => 'Vis brugerprofil'))); ?></span> 
+    </div>
+    <? endforeach; ?>
+  <? else: ?>
+  Du har ingen ubesvarede høringer.
+  <? endif; ?>
+</div>
+<!-- /block-hearing-unresponded.tpl.php -->
