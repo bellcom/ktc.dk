bcf1f42c49d1d3ffa41dc0b5060449442c22abd5
Thomas Thune Hansen <tth@bellcom.dk>
2015-01-13 11:42:51 +0100

    Dont shorthand php tags

--- public_html/sites/all/modules/ktc_hearing/block-hearing-unresponded.tpl.php
+++ public_html/sites/all/modules/ktc_hearing/block-hearing-unresponded.tpl.php
@@ -8,18 +8,21 @@
 ?>
 <!-- block-hearing-unresponded.tpl.php -->
 <div class="view-content">
-  <? if (!empty($unresponded_hearings)): ?>
-    <? foreach($unresponded_hearings as $hearing): ?>
+  <?php if (!empty($unresponded_hearings)): ?>
+    <?php foreach($unresponded_hearings as $hearing): ?>
 
     <div class="views-row">
       <div class="created"><?php print date('d. F Y', $hearing->created); ?></div>
-      <h5 class="field-content"><? print l($hearing->title, 'node/' . $hearing->nid); ?></h5>
+      <h5 class="field-content"><?php print l($hearing->title, 'node/' . $hearing->nid); ?></h5>
 
-      <span class="field-content">Af <? print l($hearing->user_mail, 'user/' . $hearing->uid, array('attributes' => array('class' => array('username'), 'title' => 'Vis brugerprofil'))); ?></span> 
+      <span class="field-content">Af <?php print l($hearing->user_mail, 'user/' . $hearing->uid, array('attributes' => array('class' => array('username'), 'title' => 'Vis brugerprofil'))); ?></span> 
     </div>
-    <? endforeach; ?>
-  <? else: ?>
+    <?php endforeach; ?>
+  <?php else: ?>
   Du har ingen ubesvarede høringer.
-  <? endif; ?>
+  <?php endif; ?>
+</div>
+<div class="view-footer">
+  <p><a href="/hoeringer">Se flere</a></p>
 </div>
 <!-- /block-hearing-unresponded.tpl.php -->
