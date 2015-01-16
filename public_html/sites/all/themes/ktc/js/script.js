1e2bfe136802a8881a5043dd3ba16c9848358e41
Yani0029 <yani@bellcom.dk>
2015-01-16 13:53:54 +0100

    Adjust js for search page

--- public_html/sites/all/themes/ktc/js/script.js
+++ public_html/sites/all/themes/ktc/js/script.js
@@ -170,7 +170,7 @@
     });
 
     // Search page.
-    var $search_container = $('.view-Search.view-id-Search')
+    var $search_container = $('.view-Search.view-id-Search .view-content')
 
     $search_container.imagesLoaded(function(){
       $search_container.masonry({
