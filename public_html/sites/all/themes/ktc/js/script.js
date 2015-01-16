caab0ccd673086430af338216d4c5a79573a7b84
Yani0029 <yani@bellcom.dk>
2015-01-16 14:21:09 +0100

    Adjust user link in navigation region

--- public_html/sites/all/themes/ktc/js/script.js
+++ public_html/sites/all/themes/ktc/js/script.js
@@ -51,6 +51,12 @@
 
       }
     });
+    if ($('.header-user-name a.user-name').width() > 130) {
+      $('.header-user-name a.user-name').css('font-size', '14px');
+    }
+    if ($('.header-user-name a.user-name').width() > 180) {
+      $('.header-user-name a.user-name').css('font-size', '13px');
+    }
     // borger.dk articles
     $('div.mArticle').hide();
     $('.microArticle a.gplus').click(function() {
