06005bbe387ea8514b17386e06c458280fe944e6
Thomas Thune Hansen <tth@bellcom.dk>
2015-01-20 07:13:09 +0100

    Fetch memberships from webservice

--- public_html/sites/all/modules/custom/ktc_crm/test_update_groups.php
+++ public_html/sites/all/modules/custom/ktc_crm/test_update_groups.php
@@ -4,12 +4,14 @@
 
 
 $groups = ktc_crm_fetch_groupmembers();
-ktc_crm_group_create_array($groups);
-error_log(__FILE__ . ' : ' . __LINE__ . ': ' .  print_r($groups, 1));
-return;
-$groups = ktc_crm_fetch_grouproles();
-ktc_crm_group_create_array($groups);
+$group_members = ktc_crm_group_create_array($groups);
+ktc_crm_group_parse_memberships($group_members);
 //error_log(__FILE__ . ' : ' . __LINE__ . ': ' .  print_r($groups, 1));
+return;
+
+$groups = ktc_crm_fetch_grouproles();
+//ktc_crm_group_update_userroles($groups);
+error_log(__FILE__ . ' : ' . __LINE__ . ': ' .  print_r($groups, 1));
 return;
 
 foreach($groups->Results->GroupDto as $groupdto) {
