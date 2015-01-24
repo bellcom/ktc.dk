086a48cf34a6e99877ad3ce5031aa791afa8b801
Thomas Thune Hansen <tth@bellcom.dk>
2015-01-24 16:34:55 +0100

    Group handling

--- public_html/sites/all/modules/ktc_crm/test_update_groups.php
+++ public_html/sites/all/modules/ktc_crm/test_update_groups.php
@@ -1,21 +1,17 @@
 <?php
 
-//$groups = ktc_crm_fetch_groups();
-
-
-$groups = ktc_crm_fetch_groupmembers();
-$group_members = ktc_crm_group_create_array($groups);
-ktc_crm_group_parse_memberships($group_members);
-ktc_crm_group_remove_old_memberships($group_members);
-
-$grouproles = ktc_crm_fetch_grouproles();
-ktc_crm_group_update_userroles($grouproles);
-
-return;
-error_log(__FILE__ . ' : ' . __LINE__ . ': ' .  print_r($groups, 1));
-return;
+$groups = ktc_crm_fetch_groups();
 
 foreach($groups->Results->GroupDto as $groupdto) {
-  error_log(__FILE__ . ' : ' . __LINE__ . ': ' .  print_r($groupdto, 1));
   ktc_crm_group_update_from_groupdto($groupdto);
 }
+
+//$groups = ktc_crm_fetch_groupmembers();
+//$group_members = ktc_crm_group_create_array($groups);
+//ktc_crm_group_parse_memberships($group_members);
+//ktc_crm_group_remove_old_memberships($group_members);
+
+//$grouproles = ktc_crm_fetch_grouproles();
+//ktc_crm_group_update_userroles($grouproles);
+
+
