34f04c4ba0d865408b393b358f851a0c9e3b76ab
Thomas Thune Hansen <tth@bellcom.dk>
2015-02-06 15:15:12 +0100

    KTC member stuff, and only showing relevant level to user

--- public_html/sites/all/modules/ktc_crm/ktc-crm-subscriptions-page.tpl.php
+++ public_html/sites/all/modules/ktc_crm/ktc-crm-subscriptions-page.tpl.php
@@ -14,7 +14,9 @@
       $product_uuid = field_get_items('node', $node, 'field_crm_uuid');
       if ($product_uuid && isset($user_subscriptions[$product_uuid[0]['value']])) :
         if ($user_subscriptions[$product_uuid[0]['value']]->is_active):
-          print l(t('Opsig'), current_path() . '/' . $node->nid . '/cancel', array('attributes' => array('class' => 'pull-right btn btn-warning')));
+          if (!_ktc_crm_is_ktc_member()):
+            print l(t('Opsig'), current_path() . '/' . $node->nid . '/cancel', array('attributes' => array('class' => 'pull-right btn btn-warning')));
+          endif;
           print l(t('Rediger'), current_path() . '/' . $node->nid . '/edit', array('attributes' => array('class' => 'pull-right btn btn-success')));
         else:
           print l(t('Opret'), current_path() . '/' . $node->nid . '/add', array('attributes' => array('class' => 'pull-right btn btn-success')));
