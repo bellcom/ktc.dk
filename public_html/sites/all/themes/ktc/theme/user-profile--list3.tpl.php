67cd396042f14b575be70a5fb885489484665b19
Franz Skaaning <fs@bellcom.dk>
2015-03-31 17:52:16 +0200

    Liste 3 tpl

---
+++ public_html/sites/all/themes/ktc/theme/user-profile--list3.tpl.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to present all user profile data.
+ *
+ * This template is used when viewing a registered member's profile page,
+ * e.g., example.com/user/123. 123 being the users ID.
+ *
+ * Use render($user_profile) to print all profile items, or print a subset
+ * such as render($user_profile['user_picture']). Always call
+ * render($user_profile) at the end in order to print all remaining items. If
+ * the item is a category, it will contain all its profile items. By default,
+ * $user_profile['summary'] is provided, which contains data on the user's
+ * history. Other data can be included by modules. $user_profile['user_picture']
+ * is available for showing the account picture.
+ *
+ * Available variables:
+ *   - $user_profile: An array of profile items. Use render() to print them.
+ *   - Field variables: for each field instance attached to the user a
+ *     corresponding variable is defined; e.g., $account->field_example has a
+ *     variable $field_example defined. When needing to access a field's raw
+ *     values, developers/themers are strongly encouraged to use these
+ *     variables. Otherwise they will have to explicitly specify the desired
+ *     field language, e.g. $account->field_example['en'], thus overriding any
+ *     language negotiation rule that was previously applied.
+ *
+ * @see user-profile-category.tpl.php
+ *   Where the html is handled for the group.
+ * @see user-profile-item.tpl.php
+ *   Where the html is handled for each item in the group.
+ * @see template_preprocess_user_profile()
+ *
+ * @ingroup themeable
+ */
+?>
+<section class="user-teaser col-md-12 no-padding clearfix">
+  <div><h2>gduygsdygqd8ygdg</h2></div>
+  
+  <div class="col-md-3 col-sm-3 col-xs-3 no-padding">
+    <?php global $user; $account = $elements['#account']; ?>
+    <?php if ($user->uid == $account->uid): ?>
+      <div class="user-image ktc-red">
+	       
+    <?php else: ?>
+      <div class="user-image">
+    <?php endif; ?>
+      <?php print render($user_profile['user_picture']); ?>
+    </div>
+  </div>
+  <div class="col-md-9 col-sm-9 col-xs-9">
+    <div class="user-body">
+      <div class="employer">
+        <?php print $employer; ?>
+      </div>
+      <div class="user-name">
+        <?php print $user_name; ?>
+      </div>
+      <span><?php print $job_title; ?></span>
+    </div>
+  </div>
+
+</section>
