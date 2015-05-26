2a4e8f241dace76585e8760024f259f181250b97
Thomas Thune Hansen <tth@bellcom.dk>
2015-05-26 14:00:36 +0200

    KTC CRM - add group sync to cron script

--- public_html/sites/all/modules/ktc_crm/ktc_crm.cron.php
+++ public_html/sites/all/modules/ktc_crm/ktc_crm.cron.php
@@ -6,4 +6,8 @@
  * Here we call the wanted sync functions.
  */
 
+// Update users from CRM.
 ktc_crm_webuser_update_users();
+
+// Update groups, grouproles and groupmembers from CRM.
+ktc_crm_group_update_groups();
