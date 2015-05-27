5989fc95e52837d9aa04971c051b998b1b9ace4e
Thomas Thune Hansen <tth@bellcom.dk>
2015-05-27 20:07:23 +0200

    Data import - only import nodes from january foward

--- public_html/ktc_php_script/functions.php
+++ public_html/ktc_php_script/functions.php
@@ -9,6 +9,7 @@ function get_hearing_elements_from_db_table($table, $condition = NULL) {
   if (isset($condition)) {
     $query->condition($condition, '', '<>');
   }
+  $query->condition('tstamp', strtotime('2015-01-01 00:00:01'), '>');
   $result = $query->execute();
   while ($record = $result->fetchAssoc()) {
     $elements[] = $record;
