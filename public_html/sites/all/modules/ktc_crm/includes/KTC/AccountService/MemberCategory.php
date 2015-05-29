6cb162f9bf973573c6be688fd09a1362577f15f0
Thomas Thune Hansen <tth@bellcom.dk>
2015-05-29 14:38:21 +0200

    KTC CRM - update webservice classes

---
+++ public_html/sites/all/modules/ktc_crm/includes/KTC/AccountService/MemberCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace KTC\AccountService;
+
+class MemberCategory
+{
+  const __default = 'DirektoerDirektionsmedlem';
+  const DirektoerDirektionsmedlem = 'DirektoerDirektionsmedlem';
+  const DirektoerDirektionsmedlemSuppl = 'DirektoerDirektionsmedlemSuppl';
+  const AndreCheferMedRefTil1A = 'AndreCheferMedRefTil1A';
+  const DirektoerOeversteChefFaellesKommunalSelskab = 'DirektoerOeversteChefFaellesKommunalSelskab';
+  const AndreCheferMedRefTil1C = 'AndreCheferMedRefTil1C';
+  const DirektoerI100ProcentKommEjetSelskab = 'DirektoerI100ProcentKommEjetSelskab';
+  const FormandAssocieringForening = 'FormandAssocieringForening';
+  const SaerligFagkundskabEllerLedendeRolle = 'SaerligFagkundskabEllerLedendeRolle';
+  const Senior = 'Senior';
+
+
+}
