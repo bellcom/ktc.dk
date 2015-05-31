fad5b06dc5d42da2500a7d83e66e9e465910dc03
Morten Nissen <mn@bellcom.dk>
2015-05-31 20:20:49 +0200

    Changes

--- public_html/sites/all/themes/ktc/theme/user/user-profile--teaser.tpl.php
+++ public_html/sites/all/themes/ktc/theme/user/user-profile--teaser.tpl.php
@@ -47,9 +47,19 @@ $account = $elements['#account'];
 
   <!-- Begin - profile content -->
   <div class="ktc-user-profile-content">
+
+    <?php if (isset($user_name)): ?>
     <h5><?php print $user_name; ?></h5>
+    <?php endif ?>
+
+    <?php if (isset($employer)): ?>
     <p><?php print $employer; ?></p>
+    <?php endif ?>
+
+    <?php if (isset($job_title)): ?>
     <p><?php print $job_title; ?></p>
+    <?php endif ?>
+
   </div>
   <!-- End - profile content -->
 
