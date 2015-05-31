6bb155e91a02038197c50af9153203fb31ee9084
Morten Nissen <mn@bellcom.dk>
2015-05-31 09:03:56 +0200

    Add new block-footer1 tpl file and some override less files

---
+++ public_html/sites/all/themes/ktc/theme/block--footer1.tpl.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a block.
+ *
+ * Available variables:
+ * - $block->subject: Block title.
+ * - $content: Block content.
+ * - $block->module: Module that generated the block.
+ * - $block->delta: An ID for the block, unique within each module.
+ * - $block->region: The block region embedding the current block.
+ * - $classes: String of classes that can be used to style contextually through
+
+ * Helper variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $block_zebra: Outputs 'odd' and 'even' dependent on each block region.
+ * - $zebra: Same output as $block_zebra but independent of any block region.
+ * - $block_id: Counter dependent on each block region.
+ * - $id: Same output as $block_id but independent of any block region.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ * - $block_html_id: A valid HTML ID and guaranteed unique.
+ *
+ * @see bootstrap_preprocess_block()
+ * @see template_preprocess()
+ * @see template_preprocess_block()
+ * @see bootstrap_process_block()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?>
+
+<!-- Begin - block -->
+<aside class="<?php print $classes; ?> ktc-block" id="<?php print $block_html_id; ?>" <?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if ($block->subject): ?>
+    <!-- Begin - heading -->
+    <div class="ktc-block-heading">
+      <h3 class="ktc-block-title"><?php print $block->subject ?></h3>
+    </div>
+    <!-- End - heading -->
+  <?php endif;?>
+  <?php print render($title_suffix); ?>
+
+  <!-- Begin - content -->
+  <div class="ktc-block-content"><?php print $content ?></div>
+  <!-- End - content -->
+
+</aside>
+<!-- End - block -->
