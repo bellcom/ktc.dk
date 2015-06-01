810f8ffd24792b89d9cd78dbbe82a815965e48fc
Morten Nissen <mn@bellcom.dk>
2015-06-01 21:16:17 +0200

    Fixes

--- public_html/sites/all/themes/ktc/theme/panels/panels-pane--comments-list.tpl.php
+++ public_html/sites/all/themes/ktc/theme/panels/panels-pane--comments-list.tpl.php
@@ -16,7 +16,6 @@
  * - $display: The complete panels display object containing all kinds of
  *   data including the contexts and all of the other panes being displayed.
  */
-xdebug_break();
 ?>
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>
