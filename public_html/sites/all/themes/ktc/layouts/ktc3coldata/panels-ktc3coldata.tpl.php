51ece0d98cbfd71b91fd8fac8d3dfab1c8ea5039
Morten Nissen <mn@bellcom.dk>
2015-06-01 20:37:01 +0200

    Fixes

--- public_html/sites/all/themes/ktc/layouts/ktc3coldata/panels-ktc3coldata.tpl.php
+++ public_html/sites/all/themes/ktc/layouts/ktc3coldata/panels-ktc3coldata.tpl.php
@@ -46,23 +46,17 @@
         </div>
       <?php endif; ?>
 
-      <?php if ($content['centerleft'] || $content['centerright'] || $content['center']): ?>
+      <?php if ($content['centerleft'] || $content['centerright']): ?>
         <div class="row">
 
           <?php if ($content['centerleft']): ?>
-            <div class="col-sm-6 col-md-3 pane-centerleft">
+            <div class="col-sm-6 pane-centerleft">
               <?php print $content['centerleft']; ?>
             </div>
           <?php endif ?>
 
-          <?php if ($content['center']): ?>
-            <div class="col-xs-12 col-md-6 pane-center">
-              <?php print $content['center']; ?>
-            </div>
-          <?php endif ?>
-
           <?php if ($content['centerright']): ?>
-            <div class="col-sm-6 col-md-3 pane-centerright">
+            <div class="col-sm-6 pane-centerright">
               <?php print $content['centerright']; ?>
             </div>
           <?php endif ?>
