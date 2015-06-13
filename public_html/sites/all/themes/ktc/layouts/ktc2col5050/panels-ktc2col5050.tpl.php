0943d22a699a1e9a7be2f80dd2443af31bfbc4de
Franz Skaaning <fs@bellcom.dk>
2015-06-13 12:46:17 +0200

    Table layout til høringer

---
+++ public_html/sites/all/themes/ktc/layouts/ktc2col5050/panels-ktc2col5050.tpl.php
@@ -0,0 +1,20 @@
+<div class="row ktc2col5050" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="col-md-6 col-sm-6 col-xs-12">
+    <div id="panel-pane-ktc2col5050-left">
+      <?php print $content['content']; ?>
+    </div>
+  </div>
+  <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="row">
+		<div class="col-sm-12" id="panel-pane-ktc2col5050-right-top">
+			<?php print $content['contentrighttop']; ?>
+    	</div>
+	    <div class="col-sm-6" id="panel-pane-ktc2col5050-right-bot-left">
+			<?php print $content['contentrightbotleft']; ?>		    
+	    </div>
+	    <div class="col-sm-6" id="panel-pane-ktc2col5050-right-bot-right">
+			<?php print $content['contentrightbotright']; ?>
+	    </div>
+    </div>
+  </div>
+</div>
