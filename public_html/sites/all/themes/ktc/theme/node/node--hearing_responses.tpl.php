fd54f54597a8761c4b9785544c371d026cf4e4f9
Thomas Thune Hansen <tth@bellcom.dk>
2015-06-24 14:06:28 +0200

    Hearings - Response, add some markup and styling

---
+++ public_html/sites/all/themes/ktc/theme/node/node--hearing_responses.tpl.php
@@ -0,0 +1,35 @@
+<?php if ($content['field_is_no_answer']['#items'][0]['value']) : ?>
+  <div class="ktc-aside">
+    <div class="ktc-aside-heading">
+      <h3 class="ktc-aside-title"><?php print $node->name; ?> - Svarer ikke</h3>
+    </div>
+  </div>
+
+<?php else: ?>
+  <div class="ktc-aside ktc-aside-toggle closed">
+    <div class="ktc-aside-heading">
+      <h3 class="ktc-aside-title"><?php print $node->name; ?></h3>
+    </div>
+    <div class="ktc-aside-body pane-content">
+      <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+        <div class="wrap">
+          <?php
+            // Hide comments, tags, and links now so that we can render them
+            // later.
+            hide($content['comments']);
+            hide($content['links']);
+            hide($content['field_tags']);
+            hide($content['field_gammel_gid']);
+            hide($content['field_gammel_nid']);
+            hide($content['field_gammel_typo3_id']);
+            hide($content['field_news_author']);
+
+            print render($content);
+
+          ?>
+
+        </div>
+      </article>
+    </div>
+  </div>
+<?php endif; ?>
