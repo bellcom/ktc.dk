9b893084da6607cdd8a13c7c2eda0b027fe4a374
Morten Nissen <mn@bellcom.dk>
2015-06-26 09:49:00 +0200

    User rings, and attachments fix

--- public_html/sites/all/themes/ktc/theme/node/teaser-large/node--os2web_base_news--teasercomments.tpl.php
+++ public_html/sites/all/themes/ktc/theme/node/teaser-large/node--os2web_base_news--teasercomments.tpl.php
@@ -33,7 +33,6 @@
             <a href="<?php global $base_url; print $base_url . $node_url; ?>#comments" data-toggle="tooltip" data-placement="bottom" title="Kommentarer" class="ktc-footer-button ktc-footer-button-comment"><?php print $num_comments; ?></a>
             <span data-toggle="tooltip" data-placement="bottom" title="Visninger" class="ktc-footer-button ktc-footer-button-viewers"><?php print $statistics_count; ?></span>
             <span class="ktc-footer-button ktc-footer-button-<?php print $type; ?> pull-right"><?php print node_type_get_name($type); ?></span>
-            <span class="ktc-footer-button pull-right"><?php print t('Bilag:') . ' ' . $num_attachments; ?></span>
         </div>
 
     </article>
