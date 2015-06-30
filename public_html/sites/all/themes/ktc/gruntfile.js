082c76087e832f6d86127c286b90131545c5d932
Morten Nissen <mn@bellcom.dk>
2015-06-30 10:31:05 +0200

    Change sprite padding to an even number

--- public_html/sites/all/themes/ktc/gruntfile.js
+++ public_html/sites/all/themes/ktc/gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
         src: 'images/sprites/*.png',
         destImg: 'images/sprites.png',
         destCSS: 'less/sprites.less',
-        padding: 5,
+        padding: 6,
         cssFormat: 'less',
         cssOpts: {
           cssClass: function (item) {
