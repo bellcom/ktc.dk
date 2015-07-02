7972d6774731c83486927e6e29c8e7ec365996d0
Morten Nissen <mn@bellcom.dk>
2015-07-02 14:02:57 +0200

    Teaser 2 document attachments and commentlist

--- public_html/sites/all/themes/ktc/theme/node/teaser-large/node--document--teasercomments.tpl.php
+++ public_html/sites/all/themes/ktc/theme/node/teaser-large/node--document--teasercomments.tpl.php
@@ -2,45 +2,47 @@
 global $base_url;
 ?>
 
-<?php xdebug_break(); if (!$page): ?>
-    <!-- Begin - teaser large -->
-    <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>  ktc-teaser-large"<?php print $attributes; ?>>
+<?php if (!$page): ?>
+  <!-- Begin - teaser large -->
+  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>  ktc-teaser-large"<?php print $attributes; ?>>
 
-        <!-- Begin - heading -->
-        <div class="ktc-teaser-large-heading">
-            <span><?php print $created_ago . ' ' . t('siden'); ?></span>
-            <?php print $user_name; ?>
+    <!-- Begin - heading -->
+    <div class="ktc-teaser-large-heading">
+      <span><?php print $created_ago . ' ' . t('siden'); ?></span>
+      <?php print $user_name; ?>
+    </div>
+    <!-- End - heading -->
+
+    <div class="ktc-teaser-large-body">
+
+      <h4 class="ktc-teaser-large-body-title">
+        <a href="<?php print $base_url . $node_url; ?>"><?php print $title_shortened; ?></a>
+      </h4>
+
+      <p><?php print $body_shortened; ?></p>
+
+      <?php if ($content['field_dokument']): ?>
+        <div class="ktc-aside-table-list">
+          <?php print render($content['field_dokument']); ?>
         </div>
-        <!-- End - heading -->
+      <?php endif; ?>
 
-        <div class="ktc-teaser-large-body">
-
-            <h4 class="ktc-teaser-large-body-title"><a href="<?php print $base_url . $node_url; ?>"><?php print $title_shortened; ?></a></h4>
-
-            <p><?php print $body_shortened; ?></p>
-
-            <div class="ktc-comments-list">
-              <div class="ktc-comments-list-body">
-                <?php if (isset($comments_view)): ?>
-                  <?php print $comments_view; ?>
-                <?php endif ?>
-              </div>
-            </div>
-
-            <?php if (isset($field_dokument)): ?>
-            <div class="ktc-aside-table-list">
-              <?php print render($field_dokument); ?>
-            </div>
-            <?php endif; ?>
-
+      <div class="ktc-comments-list">
+        <div class="ktc-comments-list-body">
+          <?php if (isset($comments_view)): ?>
+            <?php print $comments_view; ?>
+          <?php endif ?>
         </div>
+      </div>
 
-        <div class="ktc-footer">
-            <a href="<?php print $base_url . $node_url; ?>#comments" data-toggle="tooltip" data-placement="bottom" title="Kommentarer" class="ktc-footer-button ktc-footer-button-comment"><?php print $num_comments; ?></a>
-            <span data-toggle="tooltip" data-placement="bottom"title="Visninger" class="ktc-footer-button ktc-footer-button-viewers"><?php print $statistics_count; ?></span>
-            <span class="ktc-footer-button ktc-footer-button-<?php print $type; ?> pull-right"><?php print node_type_get_name($type); ?></span>
-        </div>
+    </div>
 
-    </article>
-    <!-- End - teaser large -->
+    <div class="ktc-footer">
+      <a href="<?php print $base_url . $node_url; ?>#comments" data-toggle="tooltip" data-placement="bottom" title="Kommentarer" class="ktc-footer-button ktc-footer-button-comment"><?php print $num_comments; ?></a>
+      <span data-toggle="tooltip" data-placement="bottom" title="Visninger" class="ktc-footer-button ktc-footer-button-viewers"><?php print $statistics_count; ?></span>
+      <span class="ktc-footer-button ktc-footer-button-<?php print $type; ?> pull-right"><?php print node_type_get_name($type); ?></span>
+    </div>
+
+  </article>
+  <!-- End - teaser large -->
 <?php endif; ?>
