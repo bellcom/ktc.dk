544a8526d6e68ecdeed69b7faf72f6df36f0be62
Thomas Thune Hansen <tth@bellcom.dk>
2015-07-02 10:51:02 +0200

    Hearing - add form for kl answer on summary node view

--- public_html/sites/all/themes/ktc/theme/node/node--hearing_responses.tpl.php
+++ public_html/sites/all/themes/ktc/theme/node/node--hearing_responses.tpl.php
@@ -1,35 +1,53 @@
-<?php if ($content['field_is_no_answer']['#items'][0]['value']) : ?>
-  <div class="ktc-aside">
-    <div class="ktc-aside-heading">
-      <h3 class="ktc-aside-title"><?php print $node->name; ?> - Svarer ikke</h3>
-    </div>
-  </div>
+<?php if ($content['field_is_summary']['#items'][0]['value']) : ?>
+  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <div class="wrap">
+      <?php
+        // Hide comments, tags, and links now so that we can render them
+        // later.
+        hide($content['comments']);
+        hide($content['links']);
+        hide($content['field_tags']);
+        hide($content['field_gammel_gid']);
+        hide($content['field_gammel_nid']);
+        hide($content['field_gammel_typo3_id']);
+        hide($content['field_news_author']);
+        print render($content);
+      ?>
 
+    </div>
+  </article>
 <?php else: ?>
-  <div class="ktc-aside ktc-aside-toggle closed">
-    <div class="ktc-aside-heading">
-      <h3 class="ktc-aside-title"><?php print $node->name; ?></h3>
+  <?php if ($content['field_is_no_answer']['#items'][0]['value']) : ?>
+    <div class="ktc-aside">
+      <div class="ktc-aside-heading">
+        <h3 class="ktc-aside-title"><?php print $node->name; ?> - Svarer ikke</h3>
+      </div>
     </div>
-    <div class="ktc-aside-body pane-content">
-      <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-        <div class="wrap">
-          <?php
-            // Hide comments, tags, and links now so that we can render them
-            // later.
-            hide($content['comments']);
-            hide($content['links']);
-            hide($content['field_tags']);
-            hide($content['field_gammel_gid']);
-            hide($content['field_gammel_nid']);
-            hide($content['field_gammel_typo3_id']);
-            hide($content['field_news_author']);
 
-            print render($content);
+  <?php else: ?>
+    <div class="ktc-aside ktc-aside-toggle closed">
+      <div class="ktc-aside-heading">
+        <h3 class="ktc-aside-title"><?php print $node->name; ?></h3>
+      </div>
+      <div class="ktc-aside-body pane-content">
+        <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+          <div class="wrap">
+            <?php
+              // Hide comments, tags, and links now so that we can render them
+              // later.
+              hide($content['comments']);
+              hide($content['links']);
+              hide($content['field_tags']);
+              hide($content['field_gammel_gid']);
+              hide($content['field_gammel_nid']);
+              hide($content['field_gammel_typo3_id']);
+              hide($content['field_news_author']);
+              print render($content);
+            ?>
 
-          ?>
-
-        </div>
-      </article>
+          </div>
+        </article>
+      </div>
     </div>
-  </div>
+  <?php endif; ?>
 <?php endif; ?>
