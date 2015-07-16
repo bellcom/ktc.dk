975aa19093dbfe5bf59dcca1a48acea4d5b74e41
Franz Skaaning <fs@bellcom.dk>
2015-07-16 08:46:30 +0200

    Kalender tag

--- public_html/sites/all/themes/ktc/views-view-field--add-to-outlook--panel-pane-1--nothing.tpl.php
+++ public_html/sites/all/themes/ktc/views-view-field--add-to-outlook--panel-pane-1--nothing.tpl.php
@@ -1,9 +1,2 @@
-    <script type="text/javascript">(function () {
-            if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-                var h = d[g]('body')[0];h.appendChild(s); }})();
-    </script>
+<script type="text/javascript" src="https://addthisevent.com/libs/1.6.0/ate.min.js"></script>
 <?php print $output; ?>
