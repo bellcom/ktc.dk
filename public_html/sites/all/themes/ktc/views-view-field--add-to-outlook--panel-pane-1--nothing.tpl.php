6fabd080ff1dace0d025b418613ba7ad2cef5d26
Franz Skaaning <fs@bellcom.dk>
2015-07-16 08:54:26 +0200

    fjernet csss

--- public_html/sites/all/themes/ktc/views-view-field--add-to-outlook--panel-pane-1--nothing.tpl.php
+++ public_html/sites/all/themes/ktc/views-view-field--add-to-outlook--panel-pane-1--nothing.tpl.php
@@ -1,2 +1,12 @@
 <script type="text/javascript" src="https://addthisevent.com/libs/1.6.0/ate.min.js"></script>
+<script type="text/javascript">
+		addthisevent.settings({
+			license    : "aenqc1xuszmiif6o8mtl6510",
+			css        : false,
+			outlook    : {show:true, text:"Outlook"},
+			google     : {show:true, text:"Google <em>(online)</em>"},
+			appleical  : {show:true, text:"Apple iCal"},
+			dropdown   : {order:"appleical,google,outlook"}
+		});
+		</script>
 <?php print $output; ?>
