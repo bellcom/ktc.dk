5dc267895ca2f9575f12c67b8c4fa4e4d03df882
Morten Nissen <mn@bellcom.dk>
2015-08-10 14:47:21 +0200

    Fix word wrap on old browsers

--- public_html/sites/all/themes/ktc/gruntfile.js
+++ public_html/sites/all/themes/ktc/gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
         src: 'images/sprites/*.png',
         destImg: 'images/sprites.png',
         destCSS: 'less/sprites.less',
-        padding: 6,
+        padding: 14,
         cssFormat: 'less',
         cssOpts: {
           cssClass: function (item) {
