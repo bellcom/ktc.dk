023a96ce750fc2b727b0d0fbd39df50c9a23d2d2
Ulrik Nielsen <me@ulrik.co>
2015-11-10 10:04:31 +0100

    extended socket timeout limit to prevent soap requests from dying

--- public_html/sites/all/modules/ktc_crm/ktc_crm.cron.php
+++ public_html/sites/all/modules/ktc_crm/ktc_crm.cron.php
@@ -6,6 +6,9 @@
  * Here we call the wanted sync functions.
  */
 
+# un 2015.11.05: to prevent Soap timeouts
+ini_set('default_socket_timeout', 1200);
+
 // Update users from CRM.
 ktc_crm_webuser_update_users();
 
