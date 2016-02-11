7c52f465acf5c754c591a0e0486a29f2390dc86b
Juulia Devadze <jd@ktc>
2016-02-11 12:57:32 +0100

    Committed debug output

--- scripts/update-accounts-from-crm.php
+++ scripts/update-accounts-from-crm.php
@@ -7,7 +7,7 @@
 # un 2015.11.05: to prevent Soap timeouts
 ini_set('default_socket_timeout', 1200);
 
-echo "Updating accounts from CRM - This will take some time.";
+//cho "Updating accounts from CRM - This will take some time.";
 
 ktc_crm_account_quick_update_accounts();
 
--- scripts/update-accounts-from-crm_full.php
+++ scripts/update-accounts-from-crm_full.php
@@ -7,7 +7,7 @@
 # un 2015.11.05: to prevent Soap timeouts
 ini_set('default_socket_timeout', 1200);
 
-echo "Updating accounts from CRM - This will take some time.";
+//echo "Updating accounts from CRM - This will take some time.";
 ktc_crm_account_update_accounts();
 ktc_crm_accounts_update_affected_users();
 
