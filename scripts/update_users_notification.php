ee016a3a103c703ee92419ced7098dcfeebfb0cc
juuliabellcom <juulia@bellcom.dk>
2016-02-22 21:34:03 +0200

    Set network notification TRUE by default

---
+++ scripts/update_users_notification.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//var_dump(ktc_crm_lookup_webuser_by_mail('asdas@bellcom.ee'));
+
+$uids = array();
+$query = db_select('users', 'u');
+    $query->fields('u', array('uid'));
+    $result = $query->execute();  
+ while($record = $result->fetchAssoc()) {
+   if ($record['uid']==0)     continue;
+   $account=user_load($record['uid']);
+    $user_groups = og_get_groups_by_user($account);
+    if ($user_groups) {
+    unset($account->field_netvaerk_notifications['und']);
+    foreach ($user_groups['node'] as $nid){
+       $account->field_netvaerk_notifications['und'][]['target_id']=$nid;       
+    }    
+    user_save($account);
+    print_r('Updated user '. $account->name . '\n');
+    }
+ }
+
+    
+      
