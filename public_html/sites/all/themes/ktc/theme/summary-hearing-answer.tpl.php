2e991a830f997af8a9804f17577863f7fcfd8f6a
juuliabellcom <juulia@bellcom.dk>
2016-05-31 18:04:45 +0300

    Added  Folgevirkninger field to pdf

--- public_html/sites/all/themes/ktc/theme/summary-hearing-answer.tpl.php
+++ public_html/sites/all/themes/ktc/theme/summary-hearing-answer.tpl.php
@@ -93,6 +93,8 @@
     <?php print render(field_view_field('node', $summary_node, 'field_general_comments', array('label' => 'hidden'))); ?><br />
     <b>Detaljerede bemærkninger</b><br />
     <?php print render(field_view_field('node', $summary_node, 'body', array('label' => 'hidden'))); ?><br />
+    <b> Følgevirkninger (herunder økonomiske)</b><br />
+    <?php print render(field_view_field('node', $summary_node, 'field_consequences', array('label' => 'hidden'))); ?><br />    
     <b>Videre handlinger</b><br />
     <?php print render(field_view_field('node', $summary_node, 'field_further_action', array('label' => 'hidden'))); ?><br />
     <b>Vedhæftede fil(er)</b><br />
