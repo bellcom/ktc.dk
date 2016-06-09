7083b4d4f071ab4e2b868b35e62053e8391e994c
juuliabellcom <juulia@bellcom.dk>
2016-06-09 21:41:15 +0300

    Small changes

--- scripts/delete_duplicate_membership_from_CRM.php
+++ scripts/delete_duplicate_membership_from_CRM.php
@@ -1,7 +1,8 @@
 <?php
+
 /*
  * Script delete duplicates memberships from CRM
-*/
+ */
 $query = db_select('users', 'u');
 $query->fields('u', array('mail'));
 $result = $query->execute();
@@ -14,18 +15,22 @@ while ($record = $result->fetchAssoc()) {
     $duplicates = array();
     if (is_array($webuserdto->UdvalgsMedlemskaber->GroupMemberDto)) {
       foreach ($webuserdto->UdvalgsMedlemskaber->GroupMemberDto as $groupmember) {
-        if ($membership_exists = array_search($groupmember->Group->CrmId, $memberships)) {
-          if (!in_array(($membership_exists), $duplicates))
-            $duplicates[] = $membership_exists;
-          $duplicates[] = $groupmember->CrmId;
-        } else
-          $memberships[$groupmember->CrmId] = $groupmember->Group->CrmId;
+        if ($groupmember->IsActiveMember) {
+          if ($membership_exists = array_search($groupmember->Group->CrmId, $memberships)) {
+            if (!in_array(($membership_exists), $duplicates))
+              $duplicates[] = $membership_exists;
+            $duplicates[] = $groupmember->CrmId;
+          } else
+            $memberships[$groupmember->CrmId] = $groupmember->Group->CrmId;
+        }
       }
-    }    
+    }
+
     foreach ($duplicates as $crmuid) {
-      if (!db_query('SELECT field_crm_uuid_value  FROM {field_data_field_crm_uuid} WHERE field_crm_uuid_value = :id', array(':id' => $crmuid))->fetchField())
+      if (!db_query('SELECT field_crm_uuid_value  FROM {field_data_field_crm_uuid} INNER JOIN og_membership ON entity_id=id WHERE field_crm_uuid_value = :id', array(':id' => $crmuid))->fetchField()) {
         ktc_crm_group_membership_delete($crmuid);
-       print 'Deleted membership' .  $crmuid;
-    }    
+        print $record['mail'] . ' Deleted membership: ' . $crmuid . PHP_EOL;
+      }
+    }
   }
 }
