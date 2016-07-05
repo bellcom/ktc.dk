890fadb3df16e2cca6b59abf984ce64eec1af4b6
Morten Nissen <mn@bellcom.dk>
2016-07-05 14:11:25 +0200

    Make hearing work

--- public_html/sites/all/modules/ktc_hearing/ktc_hearing_attendees/ktc_hearing_attendees.js
+++ public_html/sites/all/modules/ktc_hearing/ktc_hearing_attendees/ktc_hearing_attendees.js
@@ -61,12 +61,14 @@ jQuery(document).ready(function($){
       $.each(selected_groups, function(i, val) {
         if ( change_groups === null || change_groups.indexOf(val) == -1) {
           $.getJSON('/ktc_hearing_attendees/get_group_members/' + val + '/' + change_groups, function(data) {
-            if (data.length > 0) {
+            if (data) {
               $.each(data, function (field, attendees) {
                 var $select = $('#edit-field-' + field + ' .chosen-entityreference-container select');
-                $.each(attendees, function (i, val) {
-                  $('#edit-field-' + field + ' .chosen-entityreference-container select option[value="' + i + '"]').remove();
-                });
+                if (attendees) {
+                  $.each(attendees, function (i, val) {
+                    $('#edit-field-' + field + ' .chosen-entityreference-container select option[value="' + i + '"]').remove();
+                  });
+                }
                 if (typeof $select.attr('multiple') == 'undefined') {
                   if (!$select.val().length) {
                     $select.find('option:nth-child(2)').attr('selected', 'selected');
@@ -142,25 +144,26 @@ jQuery(document).ready(function($){
 
   function addGroupMembers(group_id, selected, selected_groups) {
     $.getJSON('/ktc_hearing_attendees/get_group_members/' + group_id + '/' + selected_groups, function(data) {
-      if (data.length > 0) {
+      if (data) {
         $.each(data, function (field, attendees) {
           var $select = $('#edit-field-'+field+' .chosen-entityreference-container select');
+          if (attendees) {
+            $.each(attendees, function (i, val) {
 
-          $.each(attendees, function (i, val) {
+              if (!optionExists($select, i)) {
+                var $option = $('<option />', {
+                  value: i,
+                  text: val
+                });
 
-            if (!optionExists($select, i)) {
-              var $option = $('<option />', {
-                value: i,
-                text: val
-              });
+                if ($select.attr('multiple')) {
+                  $option.attr('selected', 'selected');
+                }
 
-              if ($select.attr('multiple')) {
-                $option.attr('selected', 'selected');
+                $select.append($option);
               }
-
-              $select.append($option);
-            }
-          });
+            });
+          }
 
           if (typeof $select.attr('multiple') == 'undefined') {
             if (!$select.val().length) {
@@ -169,9 +172,9 @@ jQuery(document).ready(function($){
           }
 
         });
-
-        $(".chosen-entityreference-container select").trigger("chosen:updated");
       }
+
+      $(".chosen-entityreference-container select").trigger("chosen:updated");
     });
   }
 
