c301becf036fece623f3546007bdb164bceb3d56
Mathias Larsen <tripox@tripox.dk>
2016-08-17 15:41:54 +0200

    Fix unpublish on hearing edit

--- public_html/sites/all/modules/ktc_hearing/ktc_hearing_node_submit.js
+++ public_html/sites/all/modules/ktc_hearing/ktc_hearing_node_submit.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -25,10 +25,10 @@
             }
         });
 
-            $('#hearing-node-form .form-submit').click(function(event) {
+            $('#hearing-node-form #edit-submit').click(function(event) {
                 event.preventDefault();
                 var isError = false;
-      
+
                 //event.preventDefault();
                 //alert($('#edit-field-official-responsedate-und-0-value-datepicker-popup-0').val() + ' ' + $('#edit-field-official-responsedate-und-0-value-timeEntry-popup-1').val());
                 official_date_parts = $('#edit-field-official-date-und-0-value-datepicker-popup-0').val().split('/');
@@ -37,7 +37,7 @@
                 attendee_deadlines = new Date(attendee_deadlines_parts[1] + '/' + attendee_deadlines_parts[0] + '/' + attendee_deadlines_parts[2]);
                 enrollment_deadlines_parts = $('#edit-field-enrollment-deadlines-und-0-value-datepicker-popup-0').val().split('/');
                 enrollment_deadlines = new Date(enrollment_deadlines_parts[1] + '/' + enrollment_deadlines_parts[0] + '/' + enrollment_deadlines_parts[2]);
-                $("#confirm").html(''); 
+                $("#confirm").html('');
                 //$("#confirm").attr('title', '');
                 if (daysBetween(attendee_deadlines, official_date) <= 14) {
                      $("#confirm").append('<p><b>Deltagers frist</b></p><p>Du har angivet deltagernes høringsfrist, til at være mindre end 14 dage. Det kan gøre det svært for KTC at indhente tilstrækkeligt grundige høringssvar fra deltagerne. Ønsker du at fortsætte? Tryk "annuller" for at ændre datoen.</p>');
@@ -48,9 +48,9 @@
                 if (daysBetween(enrollment_deadlines, attendee_deadlines) < 3) {
                      $("#confirm").append('<p><b>KTC høringsfrist</b></p><p>Du har angivet en frist, der efterlader mindre end 3 dage til KTC, til at sammenskrive deltagernes høringssvar og færdigbehandle høringen hos faggruppeformand/-formænd. Ønsker du at fortsætte? Tryk "annuller" for at ændre datoen</p>');
                      $("#confirm").attr('title', $("#confirm").attr('title') + 'Deltagers frist');
-                     isError = true;  
+                     isError = true;
                 }
-                if (isError == true) 
+                if (isError == true)
                    $('#confirm').dialog('open');
                else
                    $('#hearing-node-form').submit();
@@ -65,7 +65,7 @@
                         return false;
                     }
                 }*/
-               
+
                 return true;
             });
         }
