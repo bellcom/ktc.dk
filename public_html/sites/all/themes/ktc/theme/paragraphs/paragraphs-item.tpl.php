4ed184762f8f8ec1de6bdbf34918d17d93577ce6
Franz Skaaning <fs@bellcom.dk>
2017-02-24 21:22:25 +0100

    tpl names

--- public_html/sites/all/themes/ktc/theme/paragraphs/paragraphs-item.tpl.php
+++ public_html/sites/all/themes/ktc/theme/paragraphs/paragraphs-item.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * Default theme implementation for a single paragraph item.
@@ -26,8 +25,18 @@
  * @see template_process()
  */
 ?>
-<div class="sdfsdf <?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php print render($content); ?>
+<div id = "ktc_paragraph_<?php print $host_entity_id ?>_<?php print $item_id ?>" class='draggable'>
+  <div class="ting ktc-article-section <?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php print render($title_prefix); ?>
+    <?php print render($title_suffix); ?>
+    <div class="content"<?php print $content_attributes; ?>>
+      <?php
+      print render($content);
+      ?>
+      <ul class="paragraphs-items-view-links">
+        <li class="edit first"><?php print render($operations['edit']) ?></li>
+        <li class="delete last"><?php print render($operations['delete']) ?></li>
+      </ul>
+    </div>
   </div>
 </div>
