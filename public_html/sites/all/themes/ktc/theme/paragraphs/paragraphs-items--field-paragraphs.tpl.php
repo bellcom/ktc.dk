fbae4323680403b243369b3193f5ed88f24e22d9
Franz Skaaning <fs@bellcom.dk>
2017-02-24 15:47:48 +0100

    Add tilbage

--- public_html/sites/all/themes/ktc/theme/paragraphs/paragraphs-items--field-paragraphs.tpl.php
+++ public_html/sites/all/themes/ktc/theme/paragraphs/paragraphs-items--field-paragraphs.tpl.php
@@ -1,3 +1,42 @@
- <?php
+<?php
+/**
+ * @file
+ * Default theme implementation for a group of paragraph items.
+ *
+ * Available variables:
+ * - $content: Rendered HTML of content items.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. By default the following classes are available, where
+ *   the parts enclosed by {} are replaced by the appropriate values:
+ *   - paragraphs-items
+ *   - paragraphs-items-{field_name}
+ *   - paragraphs-items-{field_name}-{view_mode}
+ *   - paragraphs-items-{view_mode}
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_paragraphs_items()
+ * @see template_process()
+ */
+?>
+<div <?php if (isset($operations['add'])) : ?> id = 'article-sortable' <?php endif ?> class="tings <?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php
   print $content;
   ?>
+  <?php if (isset($operations['add'])) : ?>
+    <ul class="action-links action-links-paragraphs-add dropdown">
+
+      <a class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown"> <?php print t('Add') ?>
+      </a>
+      <ul class="dropdown-menu">
+        <?php foreach ($operations['add'] as $add_link) : ?>
+          <li><?php print render($add_link) ?></li>
+        <?php endforeach; ?>
+      </ul>
+    </ul>
+  <?php endif ?>
+</div>
