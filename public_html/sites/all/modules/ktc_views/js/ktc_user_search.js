4a0071769e7cb13e06a6c1698bf3bacdb964f51b
juuliabellcom <juulia@bellcom.dk>
2017-12-28 20:22:48 +0200

    KTC-1078 Small changes in JS script

--- public_html/sites/all/modules/ktc_views/js/ktc_user_search.js
+++ public_html/sites/all/modules/ktc_views/js/ktc_user_search.js
@@ -3,50 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- 
+
 (function ($) {
-jQuery(document).ready(function($){
-  if ($('#edit-usertype').val() == 'All'){
-    $('#edit-firma-wrapper').css("visibility", "hidden");
-  }
-  else {
-    change_firma_list($('#edit-usertype').val());
-    $('#edit-firma-wrapper').css("visibility", "visible");
-    
-  }
- 
-});
+  jQuery(document).ready(function ($) {
+    if ($('#edit-usertype').val() == 'All') {
+      $('#edit-firma-wrapper').hide();
+    } else {
+      change_firma_list($('#edit-usertype').val());
+      $('#edit-firma-wrapper').show();
+
+    }
+
+  });
 
 
-  function change_firma_list(usertypeTid){
+  function change_firma_list(usertypeTid) {
     var firstLevel = Drupal.settings.ktcUsersSearch.usertypeMap[usertypeTid];
     var firma_value = $('#edit-firma').val();
-         $.get('/ajax/users_search/get_children_accounts/' + firstLevel, function(data) {
-          var select = $('#edit-firma');
-          
-          select.empty();
-           $.each(data, function(index, element) {
-            select.append('<option value="' + element.key + '">' + element.title + '</option>');
-           }); 
-           $('#edit-firma').val(firma_value);
+    $.get('/ajax/users_search/get_children_accounts/' + firstLevel, function (data) {
+      var select = $('#edit-firma');
+      select.empty();
+      $.each(data, function (index, element) {
+        select.append('<option value="' + element.key + '">' + element.title + '</option>');
       });
-      
-   }
+      $('#edit-firma').val(firma_value);
+    });
+
+  }
   Drupal.behaviors.ktcUserSearch = {
     attach: function (context, settings) {
-      $('#edit-usertype').change(function(event) {
-       if ($('#edit-usertype').val() == 'All'){
-        $('#edit-firma-wrapper').css("visibility", "hidden");
-        $('#edit-firma').val('All');
-       } 
-       else {
-        var usertypeTid = $(this).val();
-        change_firma_list(usertypeTid);
-        $('#edit-firma-wrapper').css("visibility", "visible");
-      } 
-    });
+      $('#edit-usertype').change(function (event) {
+        if ($('#edit-usertype').val() == 'All') {
+          $('#edit-firma').val('All');
+          $('#edit-firma-wrapper').hide();
+
+        } else {
+          var usertypeTid = $(this).val();
+          change_firma_list(usertypeTid);
+          $('#edit-firma-wrapper').show();
+        }
+      });
     }
-  
+
   };
 })(jQuery);
 
