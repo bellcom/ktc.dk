fbc80100ca463266bb21e5125c69b618a2160bc4
Dobrin Dobrev <ddod@2803987.no-reply.drupal.org>
2019-10-23 15:55:16 +0300

    KTC-73 - fix for paragraphs reaarangement logic on page ct

--- public_html/sites/all/themes/ktc/template.php
+++ public_html/sites/all/themes/ktc/template.php
@@ -1032,6 +1032,8 @@ function ktc_field($variables) {
   //we not need field-items class on article page
  if ($variables["element"]["#field_name"] == 'field_paragraphs' && $variables["element"]["#bundle"] == 'artikler' )
     return;
+  if ($variables["element"]["#field_name"] == 'field_paragraphs' && $variables["element"]["#bundle"] == 'page' )
+    return;
   // Render the label, if it's not hidden.
   if (!$variables['label_hidden']) {
     $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
